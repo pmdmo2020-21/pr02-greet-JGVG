ad2168009ae91ad40ffb84c99317347fd493ac14
José Gabriel Vázquez Gil <josegabrielvazquezgil.saladillo@gmail.com>
2020-09-30 18:56:30 +0200

    Modification of "strings.xml"

--- app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import es.iessaladillo.pedrojoya.greet.databinding.MainActivityBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MainActivityBinding b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        b = MainActivityBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
     }
 
 }
